be71d66063750c9891608f4bd3946f4015bd755e
= <rk8466995@gmail.com>
2024-03-22 11:50:15 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+"use strict";
+// let numberOfFaculty: number = 4;
+// let numberOfMangement: number = 3;
+// let numberOfFacultyAbsent: number = 2;
+// let totalNumberOfFaulty: number = numberOfFaculty * 21
+// let biryani = 20;
+// console.log(biryani / totalNumberOfFaulty);
+// let fName = "Arsalsn"
+// let age = 18
+// // let message = "Hello my name is " + fName + " and my age is " + age
+// let message = `Hello my name is ${fName} and my age is ${age}. `
+// console.log(message);
+// let myName: string = "Abdul Rafay Khan";
+// let age1: number = 19;
+// let message1 = `Hello my name is ${myName} and my age is ${age1}.`;
+// console.log(message1);
+// // INCREMENT's
+// let index = 4;
+// ++index;
+// ++index;
+// ++index;
+// ++index;
+// ++index;
+// console.log(index);
+// // DECREMENT's
+// let indexMinus = 50
+// --indexMinus;
+// --indexMinus;
+// --indexMinus;
+// --indexMinus;
+// --indexMinus;
+// console.log(indexMinus);
+// let index = 10;
+// //              11   +     12  + 8 + 11 - 2        
+// let result = ++index + ++index + 8 + --index - 2;
+// console.log(result);
+// 40
+// // ASSIGNMENT OPERATORS :-
+// // 1) (Plus equals to {+=})
+// let num1 = 20;
+// num1 += 2;
+// num1 += 2;
+// num1 += 2;
+// num1 += 2;
+// console.log(num1);
+// // 2) {Minus equals to (-=)}
+// let num2 = 20;
+// num2 -= 2;
+// num2 -= 2;
+// num2 -= 2;
+// num2 -= 2;
+// console.log(num2);
+// // num1 = num1 - 22
+// COMPARISION OPERATORS :-
+// let num1 = 20;
+// let num2 = 50;
+// console.log(num1 == num2);
+// let num3 = 20;
+// let num4 = 50;
+// console.log(num3 != num4);
+// let num5 = 10;
+// let num6 = 12;
+// console.log(num5 < num6);
+// let num7 = 70;
+// let num8 = 60;
+// console.log(num7 > num8);
+let age = 18;
+console.log(age <= 18);
